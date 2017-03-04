f47ec65ae5ce36efc21a2bfe807f26146768e9b9
nkbelow <nkbelow@gmail.com>
2017-03-03 20:08:51 -0800

    Adds tables and created server connection function

--- server/db/index.js
+++ server/db/index.js
@@ -4,4 +4,16 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
+var connection = mysql.createConnection({
+    // host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database    : 'chat'
+});
+
+connection.connect(function(err) {
+    if (err) throw err;
+});
+
+module.exports = connection;
 
--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,44 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  ID integer primary key,
+  username integer,
+  messageBody text,
+  roomname integer,
+  FOREIGN KEY(username) REFERENCES usernames(id),
+  FOREIGN KEY(roomname) REFERENCES roomnames(id)
+
+
 );
 
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE roomnames (
+  /* Describe your table here.*/
+  ID integer primary key,
+  roomname text
 
+
+);
+
+CREATE TABLE usernames (
+  /* Describe your table here.*/
+  ID integer primary key,
+  username text
+
+
+);
+
+CREATE TABLE usernames_roomnames (
+  /* Describe your table here.*/
+  ID integer primary key,
+  username integer,
+  roomname integer,
+  FOREIGN KEY(username) REFERENCES usernames(id),
+  FOREIGN KEY(roomname) REFERENCES roomnames(id)
+
+
+);
 
 
 /*  Execute this file from the command line by typing:
